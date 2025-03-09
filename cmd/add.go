8aa86cb452f281a76cffffcc4adfa84cffcf33e7
AkaraChen <akarachen@outlook.com>
2025-03-09 11:27:40 +0800

    feat(cmd): add new command to create aliases for shell commands with cobra package

---
+++ cmd/add.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/akarachen/magic-alias/pkg/shell"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add <alias> <command>",
+	Short: "Add a new alias",
+	Long: `Add a new alias that will execute the specified command.
+Example: magic-alias add m git`,
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		alias := args[0]
+		command := args[1]
+
+		err := shell.AddAlias(alias, command)
+		if err != nil {
+			fmt.Printf("Error adding alias: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Successfully added alias: %s\n", alias)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
